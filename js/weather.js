6ebafc9fc3b88093d8488e38147901c237b64381
neya0 <103401991+neya0@users.noreply.github.com>
2022-04-24 22:35:34 +0900

    update weather

--- js/weather.js
+++ js/weather.js
@@ -4,7 +4,14 @@ const API_KEY = "8cefc6f3c9f616bb27f196de2c33c2bd";
 function getGeoSucess(position){
     const lat = position.coords.latitude;
     const lng = position.coords.longitude;
-    console.log("You live in", lat, lng);
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+    fetch(url).then(response => response.json()).then(data =>{
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+        
+        city.innerHTML = data.name;
+        weather.innerHTML = `${data.weather[0].main} / ${data.main.temp}`;
+    });
 }
 
 function getGeoError(){
